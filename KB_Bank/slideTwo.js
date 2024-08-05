0ec3f370bbdd50829d456ebc168b94b58b3d7f0a
dohun08 <bemik624@gmail.com>
2024-08-05 21:09:51 +0900

    슬라이드투 화면 구현

--- KB_Bank/slideTwo.js
+++ KB_Bank/slideTwo.js
@@ -4,27 +4,62 @@ const slider = document.getElementById("#slideTwo-wrap");
 const slideTwoSlider = document.querySelector("#slideTwo-slider");
 const slideTwoLis = slideTwoSlider.querySelectorAll('li')
 /* ul 넓이 계산해 주기 */
-const liWidth = slideTwoLis[0].clientWidth;
+const liWidth = 350;
 const sliderWidth = 2800;
   
 /* 리스너 설치하기 */
 let currentIdx = 0; // 슬라이드 현재 번호
 let translate = 0; // 슬라이드 위치 값
-function start(){
-    setInterval(()=> {slideTwos()}, 3000);
-}
+
+window.onload = setInterval(()=> {slideTwos()}, 1000);
+
 function slideTwos(){
-    console.log("111");
-    if(currentIdx == 8){
+    if(currentIdx == 7){
         currentIdx = 0;
         translate = -liWidth+liWidth;
-        slider.style.transform = `translateX(${translate}px)`;
-        slider.style.transition = "none";
+        
+        slideTwoSlider.style.transition = "none";
+        slideTwoSlider.style.transform = `translateX(0px)`;
+        
       }
       else {
           translate -= liWidth;
-          slider.style.transform = `translateX(${translate}px)`;
+          slideTwoSlider.style.transition = "0.5s";
+          slideTwoSlider.style.transform = `translateX(${translate}px)`;
           currentIdx+=1;
-          
+          console.log(translate);
+          console.log(currentIdx);
       }
+}
+
+
+//클릭하여 조절하기
+function left(){
+    if(currentIdx-1 == -1){
+        currentIdx = 7;
+        slideTwoSlider.style.transition = "0.5";
+        slideTwoSlider.style.transform = `translateX(-2100px)`;
+    }
+    else {
+        translate = liWidth;
+        slideTwoSlider.style.transition = "0.5s";
+        slideTwoSlider.style.transform = `translateX(${translate}px)`;
+        currentIdx-=1;
+    }
+}
+function right(){
+    if(currentIdx == 7){
+        currentIdx = 0;
+        translate = -liWidth+liWidth;
+        
+        slideTwoSlider.style.transition = "none";
+        slideTwoSlider.style.transform = `translateX(0px)`;
+    }
+    else{
+        translate -= liWidth;
+        slideTwoSlider.style.transition = "0.5s";
+        slideTwoSlider.style.transform = `translateX(${translate}px)`;
+        currentIdx+=1;
+    }
+    
 }
