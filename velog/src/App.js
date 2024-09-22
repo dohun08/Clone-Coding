4630e77045fcbc3def11cf5255b68639b917ad02
dohun08 <bemik624@gmail.com>
2024-09-22 15:27:20 +0900

    add:main데이터 불러오기 가능

--- velog/src/App.js
+++ velog/src/App.js
@@ -10,7 +10,8 @@ import ThreeCicleSrc from './assets/threeCircle.svg'
 import TrandingSrcG from './assets/trandingG.svg'
 import NewSrcG from './assets/ClockG.svg'
 import PeedG from './assets/wifiG.svg'
-import {useState} from "react"
+import Box from './components/Box'
+import React, { useEffect, useState } from 'react';
 function App() {
     const [activeIndex, setActiveIndex] = useState(0);
     
@@ -22,6 +23,28 @@ function App() {
         { src: NewSrc, src2:NewSrcG, label: '최신' },
         { src: Peed, src2:PeedG, label: '피드' }
     ];
+    const [data, setData] = useState([])
+    const [undata, setUnData] = useState([""])
+    const getData = async () => {
+        try {
+          const response = await fetch('http://localhost:3001/box');
+          const json = await response.json();
+          setData(json);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+    const unbox = ()=>{
+        setUnData(Array( 3 - (data.length%3)%3).fill(''));
+        
+    }
+      useEffect(() => {
+        getData();
+        if(data.length > 0){
+            unbox()
+        }
+
+      }, []); 
   return (
       <Body>
           <GlobalStyle />
@@ -45,8 +68,7 @@ function App() {
 
                   </Info>
               </Header>
-            <Main>
-                 <MainBar>
+              <MainBar>
                         <Nav float = {false}>
                             {trandingItems.map((item, index) => (
                                 <Tranding 
@@ -66,12 +88,24 @@ function App() {
                             </ThreeCicle>
                         </Nav>
                  </MainBar>
-                {/*    
-                    
-                
+            <Main>
+            <Main>
                 <MainSection>
-
-                </MainSection> */}
+                    {undata.length >0 ?  data.map((item) => (
+                    <Box 
+                        key={item.id}
+                        img={item.img_file}
+                        title={item.title} 
+                        description={item.description}
+                        date={item.date}
+                        name={item.user_name}
+                    />
+                    )) :null}
+                    {undata.map((_, index) => (
+                    <UnBox key={index}></UnBox> 
+                    ))}
+                </MainSection>
+                </Main>
             </Main>
           </Container>
       </Body>
@@ -80,53 +114,68 @@ function App() {
 }
 
 export default App;
+const UnBox = styled.div`
+    width: 320px;
+    height: 370px;
+`
+const MainSection = styled.section`
+    width: 66rem;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+
+`
 const ThreeCicle = styled.div`
-    width: 10%;
+    width: 1.875rem;
     display: flex;
     & > img{
         width: 15%;
         cursor: pointer;
     }
 `
-const Tranding = styled.div`
-    width: 33%;
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    border-bottom: ${(props) => (props.borderBottom ? "2px solid black" : "2px solid transparent")}; 
-    justify-content: center;
-    transition: border-bottom 0.3s ease; 
-
-    & > p {
-        color: ${(props) => (props.borderBottom ? "black" : "gray")};
-        margin-left: 5px;
-        font-size: 18px;
-        font-weight: 700;
-        transition: color 0.3s ease; 
-        cursor: pointer;
-    }
-    
-    & > img {
-        cursor: pointer;
-    }
-`;
+const Tranding = styled.div.withConfig({
+    shouldForwardProp: (prop) => !['borderBottom'].includes(prop),
+  })`
+      width: 500px;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      border-bottom: ${(props) => (props.borderBottom ? "2px solid black" : "2px solid transparent")}; 
+      justify-content: center;
+      transition: border-bottom 0.3s ease; 
+  
+      & > p {
+          color: ${(props) => (props.borderBottom ? "black" : "gray")};
+          margin-left: 5px;
+          font-size: 18px;
+          font-weight: 700;
+          transition: color 0.3s ease; 
+          cursor: pointer;
+      }
+      
+      & > img {
+          cursor: pointer;
+      }
+  `;
 
 const Main = styled.main`
     width:100%;
-    margin-top: 40px;
 `
-const Nav = styled.nav`
-    width: 25%;
+const Nav = styled.nav.withConfig({
+    shouldForwardProp: (prop) => !['float'].includes(prop),
+})`
+    width: 17.5rem;
     height: 40px;
     display: flex;
     flex-flow: row nowrap;
     justify-content: ${(props) =>(props.float ? "end" : "space-between")};
-`
+`;
 const MainBar = styled.div`
     width:100%;
     display:flex;
     flex-flow:row nowrap;
     justify-content: space-between;
+    margin: 30px 0;
 `
 const GlobalStyle = createGlobalStyle`
     *{
@@ -140,7 +189,7 @@ const GlobalStyle = createGlobalStyle`
     }
     button{cursor: pointer}
 `;
-const Body = styled.body`
+const Body = styled.div`
     margin: 0;
     padding: 0;
     width: 100%;
@@ -157,7 +206,7 @@ const Header = styled.header`
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-between;
-    margin-top: 10px;
+    padding-top: 20px;
 `
 const Logo = styled.img`
     width: 10%;
